df6ca9c68cd183891bcea679654681ff59fe4dff
FikuSystems <GabrielKo2008@outlook.com>
2024-03-24 15:43:04 +0000

    changes

--- lockkeys/Form1.cs
+++ lockkeys/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,8 +22,22 @@ namespace lockkeys
         {
             // Check and display the initial state of locking keys
             UpdateLockStates();
+            gradients();
         }
-
+        private void gradients()
+        {
+            panel1.Paint += (sender, e) =>
+            {
+                using (LinearGradientBrush brush = new LinearGradientBrush(
+                    panel1.ClientRectangle,
+                    Color.FromArgb(255, 102, 0),
+                    Color.FromArgb(179, 71, 0),
+                    LinearGradientMode.Vertical))
+                {
+                    e.Graphics.FillRectangle(brush, panel1.ClientRectangle); ;
+                }
+            };
+        }
         private void UpdateLockStates()
         {
             // Check the state of Caps Lock
@@ -42,6 +57,28 @@ namespace lockkeys
         {
             // Periodically update the state of locking keys
             UpdateLockStates();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            lockKeyCaps lockkeycaps = new lockKeyCaps
+            notifyIcon1.ShowBalloonTip(2000, "LockKeys is hidden to tray", "Double click the icon to open LockKeys settings.", ToolTipIcon.Info);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
---
+++ lockkeys/LockKeyCaps.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LockKeys
+{
+    public partial class LockKeyCaps : Form
+    {
+        public LockKeyCaps()
+        {
+            InitializeComponent();
+        }
+
+        private void LockKeyCaps_Load(object sender, EventArgs e)
+        {
+            Rectangle workingArea = Screen.PrimaryScreen.WorkingArea;
+            int xPos = workingArea.Left + (workingArea.Width - this.Width) / 2;
+            int yPos = workingArea.Top;
+            this.Location = new Point(xPos, yPos);
+        }
+    }
+}
